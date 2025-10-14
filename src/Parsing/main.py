c3423977f582e3d6772de2bfadff7c37d38bfeed
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-10-14 10:35:02 +0200

    added line break in demo printing script

--- src/Parsing/main.py
+++ src/Parsing/main.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     if data is not None:
         for key, value in data.items():
             print(f"Key={key}, Value={value}")
+            print("\n")
 
     if data is not None:
         with open('output.json', 'w') as json_file:
